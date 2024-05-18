e0186bceac1022d1f7fa0d31b0c22383b7ff2cb7
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-18 14:12:10 -0500

    I may be stupid

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
             type = "app";
             program = "${pkgs.writeScript "${name}-run" ''
               #!${pkgs.bash}/bin/bash
-              ${nodePackages_latest.nodemon}/bin/nodemon
+              ${pkgs.nodePackages_latest.nodemon}/bin/nodemon
             ''}";
           };
           run = taskFor {
